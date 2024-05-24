0e4f122f495ed910ca459d4be6638244a0f96e3a
Fabio Oliveira <fabioaurelio1269@gmail.com>
2024-05-23 23:42:22 -0300

    pick game and kill data

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+import time
+
+start_time = time.time()
+
+pattern = re.compile('\d: (.*) killed (.*) by (.*)|(InitGame)')
+
+for line in open('qgames.log', 'r'):
+    result = pattern.findall(line)
+    if result and result[0]:
+        # print(result[0])
+        killer, victim, mean, initGame = result[0]
+
+        if initGame:
+            # print("initGame")
+            continue
+
+        # print(f'killer: {killer}, victim: {victim}, mean: {mean}')
+
+print("--- %s seconds ---" % (time.time() - start_time))
